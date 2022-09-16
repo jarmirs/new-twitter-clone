82fbbc15cf105ad3bb2cd235b1e1e8bc5ceb771c
Jarmir Smith <55027346+jarmirs@users.noreply.github.com>
2022-09-16 18:02:39 -0400

    2nd comm

---
+++ src/components/Feed/TweetBox.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./Feed.css";
+import { Avatar, Button } from "@mui/material";
+import { db } from "../../firebase";
+
+function TweetBox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (e) => {
+    e.preventDefault();
+
+    db.collection("posts").add({
+      displayName: "Ozan Gürsucu",
+      username: "OznGrsc_",
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar:
+        "https://pbs.twimg.com/profile_images/1077133754345422848/v9k0AolD_400x400.jpg",
+    });
+
+    setTweetMessage("");
+    setTweetImage("");
+  };
+
+  return (
+    <div className="tweetBox">
+      <form>
+        <div className="tweetBox__input">
+          <Avatar src="https://pbs.twimg.com/profile_images/1077133754345422848/v9k0AolD_400x400.jpg" />
+          <input
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+            placeholder="What's happening?"
+            type="text"
+          />
+        </div>
+        <input
+          onChange={(e) => setTweetImage(e.target.value)}
+          value={tweetImage}
+          className="tweetBox__imageInput"
+          placeholder="Optional: Enter image URL"
+          type="text"
+        />
+
+        <Button
+          onClick={sendTweet}
+          type="submit"
+          className="tweetBox__tweetButton"
+        >
+          Tweet
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default TweetBox;
